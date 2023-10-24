f8cd5b5a05d26919c8b67add25ae6897733ff9f1
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-10-24 22:47:48 +0600

    authStore functionality added

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,11 +1,33 @@
 import { reactive } from "vue";
+import router from "../router";
 const authStore = reactive({
-  isAuthenticated: false,
-  authenticate() {
-    authStore.isAuthenticated = true;
+  isAuthenticated: localStorage.getItem("auth") == 1,
+  user: JSON.parse(localStorage.getItem("user")),
+  authenticate(email, password) {
+    fetch("http://localhost:8000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error == 0) {
+          authStore.isAuthenticated = true;
+          authStore.user = res;
+          localStorage.setItem("auth", 1);
+          localStorage.setItem("user", JSON.stringify(res));
+          router.push("/");
+        }
+      });
   },
   logout() {
     authStore.isAuthenticated = false;
+    authStore.user = {};
+    localStorage.removeItem("auth", 0);
+    localStorage.removeItem("user", "{}");
+    router.push("/login");
   },
 });
 
